c66909e78b79ee9ffad5d5c7faecfac3d562989e
Sean <sabinoriosean0@gmail.com>
2026-02-19 16:40:02 +0100

    Add execution time measurement and update README

---
+++ README.md
@@ -0,0 +1,10 @@
+
+## Branches 
+
+- `main`: Must-have implemntation 
+- `nice_to_have`: Adds CSV export and multiprocessing (performance improved from ~60s to 20s)
+
+## Performance 
+
+Sequential version: 
+~60 seconds
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 
 from src.scraper import WikipediaScraper
+import time
+def main():
+    start = time.time()
 
-def main(): 
     scraper = WikipediaScraper()
     for country in scraper.get_countries(): 
         scraper.get_leaders(country)
     scraper.get_leaders(country)
-    print("Scraping completed and saved to leaders.json")
+
+    end = time.time()
+    print(f"Scraping completed in {end - start:.2f} seconds and saved to leader.json")
 
 if __name__ == "__main__":
     main()
